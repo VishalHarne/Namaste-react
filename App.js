4d9d4e9b66a581c09489d810154198862a4b102e
Vishal Harne <vishalharne86@gmail.com>
2025-08-02 19:53:09 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,18 @@
+// const heading = React.createElement("h1", {id:"heading"}, "hello World from react");
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+//         root.render(heading);
+
+const parent = React.createElement("div",{id:"parent"},
+    [
+    React.createElement("div", {id:"child"},
+        React.createElement("h1",{},"I am a H1 Tag")
+    ),
+     React.createElement("div", {id:"child"},
+        React.createElement("h1",{},"I am a H2 Tag")
+    ),
+]);
+
+
+const root  = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
