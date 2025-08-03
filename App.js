c2fcc4bb63e38f83e3784c1001b5bc1bfb98a5b3
Vishal Harne <vishalharne86@gmail.com>
2025-08-03 11:34:44 +0530

    fixed errors

--- App.js
+++ App.js
@@ -2,19 +2,21 @@
 //         const root = ReactDOM.createRoot(document.getElementById("root"));
 //         root.render(heading);
 
+import React from "react";
+import ReactDOM from  "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},
     [
-    React.createElement("div", {id:"child"},
-        [
-        React.createElement("h1",{},"I am a H1 Tag"),
-        React.createElement("h2",{},"I am a h2 Tag")
-        ]
+    React.createElement("div", {id:"child1", key: "child1"},
+        
+        React.createElement("h1",{id:"c1"},"I am a H1 Tag ")
+       
+        
     ),
-     React.createElement("div", {id:"child"},
-             [
-        React.createElement("h1",{},"I am a H1 Tag"),
-        React.createElement("h2",{},"I am a h2 Tag")
-             ]
+     React.createElement("div", {id:"child2", key: "child2"},
+             
+        React.createElement("h1",{id:"c2"},"I am second H2 Tag")
+             
             ),
 ]);
 
