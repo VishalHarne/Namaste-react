34df178197e16cfde5b16df97195663c12b035fa
Vishal Harne <vishalharne86@gmail.com>
2025-08-03 09:32:43 +0530

    modified app.js and added parcel

--- App.js
+++ App.js
@@ -5,11 +5,17 @@
 const parent = React.createElement("div",{id:"parent"},
     [
     React.createElement("div", {id:"child"},
-        React.createElement("h1",{},"I am a H1 Tag")
+        [
+        React.createElement("h1",{},"I am a H1 Tag"),
+        React.createElement("h2",{},"I am a h2 Tag")
+        ]
     ),
      React.createElement("div", {id:"child"},
-        React.createElement("h1",{},"I am a H2 Tag")
-    ),
+             [
+        React.createElement("h1",{},"I am a H1 Tag"),
+        React.createElement("h2",{},"I am a h2 Tag")
+             ]
+            ),
 ]);
 
 
